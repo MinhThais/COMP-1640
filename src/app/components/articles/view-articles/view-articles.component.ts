1a5d83318cc6aed443a0de6a24a67b5e6aead4e1
mobile_app <duydhgcc200018@fpt.edu.vn>
2024-03-26 11:24:04 +0700

    manager with public and private

--- src/app/components/articles/view-articles/view-articles.component.ts
+++ src/app/components/articles/view-articles/view-articles.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-view-articles',
   templateUrl: './view-articles.component.html',
   styleUrls: ['./view-articles.component.css']
 })
-export class ViewArticlesComponent {
+export class ViewArticlesComponent implements OnInit{
+  public lstArticlePublic : any = [];
+  pathImg : string = "";
 
+  constructor(
+    private article:ArticleService
+  ){}
+
+
+  ngOnInit(): void {
+    this.getAllArticlesPublic();
+  }
+
+
+  getAllArticlesPublic(){
+    this.article.getAllArticlesPublic().subscribe(data => {
+      this.lstArticlePublic = data;
+      this.pathImg = this.article.Img;
+    });
+  }
 }
--- src/app/components/manager/manage-published-article/manage-published-article.component.ts
+++ src/app/components/manager/manage-published-article/manage-published-article.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-manage-published-article',
   templateUrl: './manage-published-article.component.html',
   styleUrls: ['./manage-published-article.component.css']
 })
-export class ManagePublishedArticleComponent {
+export class ManagePublishedArticleComponent implements OnInit{
+  public lstArticles : any = [];
+  pathImg : string = "";
+  status : string = "Pending";
+
+  constructor(
+    private article:ArticleService,
+    private toast:ToastrService
+  ){}
+
+  ngOnInit(): void {
+    this.getArticle();
+  }
+
+
+  getArticle(){
+    this.article.getAllArticlesSelected().subscribe(data => {
+      this.lstArticles = data;
+      this.pathImg = this.article.Img;
+    });
+  }
+
+  Public(contribution_id:number){
+    this.article.public(contribution_id).subscribe(res => {
+      this.toast.success(res.message, 'Success', {
+        timeOut: 3000,
+        progressBar:true,
+        positionClass: 'toast-top-center'
+      });
+      this.status = "Public";
+      this.ngOnInit();
+    },
+    error => {
+      this.toast.error(error.error.message, 'Error', {
+        timeOut: 3000,
+        progressBar: true,
+        positionClass: 'toast-top-center'
+      });
+    });
+  }
+
+  Private(contribution_id:number){
+    this.article.private(contribution_id).subscribe(res => {
+      this.toast.success(res.message, 'Success', {
+        timeOut: 3000,
+        progressBar:true,
+        positionClass: 'toast-top-center'
+      });
+      this.status = "Private";
+      this.ngOnInit();
+    },
+    error => {
+      this.toast.error(error.error.message, 'Error', {
+        timeOut: 3000,
+        progressBar: true,
+        positionClass: 'toast-top-center'
+      });
+    });
+  }
 
 }
