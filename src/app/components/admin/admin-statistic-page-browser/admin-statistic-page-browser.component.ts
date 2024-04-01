8b06f6861ecf6bd0f98a5a0db1fd3f4364103c44
Your Name <huynhtcgcc200065@fpt.edu.vn>
2024-04-02 05:09:46 +0700

    Desgin all statistic pages

---
+++ src/app/components/admin/admin-statistic-page-browser/admin-statistic-page-browser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-statistic-page-browser',
+  templateUrl: './admin-statistic-page-browser.component.html',
+  styleUrls: ['./admin-statistic-page-browser.component.css']
+})
+export class AdminStatisticPageBrowserComponent {
+
+}
