8eaae18a462078996d39646285a52c017e2ff65b
Your Name <huynhtcgcc200065@fpt.edu.vn>
2024-04-01 07:05:15 +0700

    Welcome 2

--- src/app/components/main/welcome/welcome.component.ts
+++ src/app/components/main/welcome/welcome.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ShareService } from 'src/app/services/share.service';
+import { UserStoreService } from 'src/app/services/user-store.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,16 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  display: string ='block'
+  public role: string = "";
+  public fullName: string ="";
+  
+  constructor(private userStore:UserStoreService, private auth:UserService, private shareService : ShareService, private router: Router,){}
 
 ngOnInit(): void {
+  this.userStore.getFullNameFromStore().subscribe(res => {
+    let fullNameFromToken = this.auth.getFullNameFormToken();
+    this.fullName = res || fullNameFromToken;
+  });
 
-}
+  this.userStore.getRoleFromStore().subscribe(res => {
+    let roleFromToken = this.auth.getRoleFromToken();
+    this.role = res || roleFromToken;
+})}
+
+
 
 onStart(){
-  this.display = 'none';
+
+  if(this.role === "Admin"){
+    this.router.navigate(['/Admin-Statistic']);
+  }
+  else if(this.role === "Coordinator"){
+    this.router.navigate(['/View-Comment']);
+    
+  }
+  else if(this.role === "Student"){
+    this.router.navigate(['/View-Student']);
+  }
+  else if(this.role === "Manager"){
+    this.router.navigate(['/View-ManagerHome']);
+  }
+  else{
+    this.router.navigate(['/View-Articles']);
+  }
 }
 
 }
+
+
 
 
